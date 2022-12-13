d27bc3d3c8ce48923d8d04394ab1284f6455646e
Raju Khattri <rajukhattri.86@gmail.com>
2022-12-13 18:42:22 +0530

    Update Model

---
+++ app/models/blog.server.ts
@@ -0,0 +1,36 @@
+import { prisma } from "~/db.server";
+
+import type { Blog } from "@prisma/client";
+
+export async function getBlogListItems() {
+  return prisma.blog.findMany({
+    select: {
+      slug: true,
+      title: true,
+    }
+  });
+}
+
+export async function getBlogs() {
+  return prisma.blog.findMany();
+}
+
+export async function getBlog(slug: string) {
+  return prisma.blog.findUnique({ where: { slug } });
+}
+
+export async function createBlog(
+  blog: Pick<Blog, "slug" | "title" | "markdown">,
+) {
+  return prisma.blog.create({ data: blog });
+}
+
+export async function deleteBlog(slug: string) {
+  return prisma.blog.delete({ where: { slug } });
+}
+
+export async function updateBlog(
+  blog: Pick<Blog, "slug" | "title" | "markdown">,
+) {
+  return prisma.blog.update({ data: blog, where: { slug: blog.slug } });
+}
