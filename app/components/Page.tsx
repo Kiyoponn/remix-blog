9d0111ef716bea016511c795dffd4e6999a2180f
Raju Khattri <rajukhattri.86@gmail.com>
2023-01-18 18:45:23 +0530

    Update

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -8,7 +8,7 @@ export default function Page({ children, title, pageinfo }: PageProps) {
   return (
     <main className='mx-auto max-w-3xl'>
       {title ? (
-        <h1 className='mt-14 mb-2 text-3xl font-extrabold'>{title}</h1>
+        <h1 className='mt-12 mb-2 text-3xl font-extrabold'>{title}</h1>
       ) : null}
       {pageinfo ? <p className='text-md text-accent-6'>{pageinfo}</p> : null}
       {children}
